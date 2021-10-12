4f21ac8a96e344cd318b6c443492cb2809d7e018
duanshuaixing <duanshuaixing@gmail.com>
2021-10-12 18:00:23 +0800

    Update config-env.sh
    update: 升级openssl从1.0.2k-fips到1.1.1i

--- ssl-alert/build-image/config-env.sh
+++ ssl-alert/build-image/config-env.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 install_crontab(){
-
+    yum clean all && yum -y makecache && yum -y update
     yum -y install openssl crontabs
+    yum -y install gcc gcc-c++ autoconf automake zlib zlib-devel pcre-devel
+    curl -LO https://www.openssl.org/source/openssl-1.1.1i.tar.gz
+    tar -xf openssl-1.1.1i.tar.gz && rm -rf openssl-1.1.1i.tar.gz
+    cd openssl-1.1.1i
+    ./config shared --openssldir=/usr/local/openssl --prefix=/usr/local/openssl
+    make -j 8
+    make install
+    rm -rf /usr/bin/openssl
+    ln -s /usr/local/openssl/bin/openssl /usr/bin/openssl
+    echo "/usr/local/openssl/lib/" >> /etc/ld.so.conf
+    ldconfig
+    openssl version
+    cd ..
+    rm -rf openssl-1.1.1i
 }
 
 config_crontab(){
