3106c7a4352f8d344becb3aae84dc9a75969cf9e
duanshuaixing <duanshuaixing@gmail.com>
2024-05-23 16:28:19 +0800

    fix: curl

--- mongodb/databases-backup/entrypoint.sh
+++ mongodb/databases-backup/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -x
-curl -I https://www.baidu.com >/dev/null  2>&1
+curl -I www.baidu.com >/dev/null  2>&1
 if [ $? -eq 0 ]; then
-    echo "Dingtalk messages can be pushed"
+    echo "The network is available"
     # 保存环境变量，开启crontab服务
     env > /etc/default/locale
     /usr/sbin/crond -i
     crontab /etc/cron.d/crontab-list
     tail -f /dev/null
 else
-    echo "network unreachable, please check network configuration! "
+    echo "The network is unreachable, please check network configuration! "
     exit 1
 fi
